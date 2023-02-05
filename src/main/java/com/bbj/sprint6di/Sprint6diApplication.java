870691dec75bcf739c4bc71ba6ccebce387c9fd9
Bruno Jullien <bruno.jullien@gmail.com>
2023-02-04 17:23:53 -0800

    project created

---
+++ src/main/java/com/bbj/sprint6di/Sprint6diApplication.java
@@ -0,0 +1,13 @@
+package com.bbj.sprint6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint6diApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprint6diApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbj/sprint6di/Sprint6diApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbj.sprint6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint6diApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
